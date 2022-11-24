17814abf4bac580fac323bd4559e865a0679b5be
Avic3nna <omarelnahhas1337@gmail.com>
2022-11-24 18:29:12 +0100

    fixed algorithm to loop through image array

--- concurrent_canny_rejection.py
+++ concurrent_canny_rejection.py
@@ -29,10 +29,10 @@ def canny_fcn(patch: np.array) -> Tuple[np.array, bool]:
     # hardcoded limit. Less or equal to 2 edges will be rejected (i.e., not saved)
     if(edge < 2.):
         #return a black image + rejected=True
-        return np.zeros_like(patch), True 
+        return (np.ones_like(patch), True)
     else:
         #return the patch + rejected=False
-        return patch, False
+        return (patch, False)
 
 
 def reject_background(img: np.array, patch_size: Tuple[int,int], step: int, save_tiles: bool = False, outdir: Path = None) -> Tuple[np.array, list, int, bool]:
@@ -42,21 +42,24 @@ def reject_background(img: np.array, patch_size: Tuple[int,int], step: int, save
     split=True
     x=(img_shape[0]//patch_size[0])*(img_shape[1]//patch_size[1])
 
-    print(f"Splitting WSI into {x} tiles for Canny background rejection...")
+    print(f"Splitting WSI into {x} tiles and Canny background rejection...")
     begin = time.time()
-
+    breakpoint()
     patches_shapes_list=[]
     # begin_time_list = []
     #changed maximum threads from 32 to os.cpu_count()
     with futures.ThreadPoolExecutor(os.cpu_count()) as executor:
         future_coords: Dict[futures.Future, int] = {}
-        for i in range(img_shape[0]//patch_size[0]):
-            for j in range(img_shape[1]//patch_size[1]):
+        i_range = range(img_shape[0]//patch_size[0])
+        j_range = range(img_shape[1]//patch_size[1])
+        for i in i_range:
+            for j in j_range:
                 patch = img[(i*patch_size[0]):(i*patch_size[0]+step), (j*patch_size[1]):(j*patch_size[1]+step)]
+                #(PIL.Image.fromarray(patch)).save(f'{outdir}/patch_{i*len(j_range) + j}.jpg')
                 patches_shapes_list.append(patch.shape)
                 future = executor.submit(canny_fcn, patch)
                 # begin_time_list.append(time.time())
-                future_coords[future] = 2*i + j # index 0 - 3. (0,0) = 0, (0,1) = 1, (1,0) = 2, (1,1) = 3
+                future_coords[future] = i*len(j_range) + j # index 0 - 3. (0,0) = 0, (0,1) = 1, (1,0) = 2, (1,1) = 3
 
     del img
     
@@ -70,7 +73,6 @@ def reject_background(img: np.array, patch_size: Tuple[int,int], step: int, save
         patch, is_rejected = tile_future.result()
         ordered_patch_list[i] = patch
         rejected_tile_list[i] = is_rejected
-    
 
     end = time.time()
     print(f"\nFinished Canny background rejection, rejected {np.sum(rejected_tile_list)} tiles: {end-begin}")
