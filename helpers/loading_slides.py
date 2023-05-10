453aa0b2b22aa3f79d58fc5dc58152a3e107fb56
Avic3nna <omarelnahhas1337@gmail.com>
2023-05-10 11:29:45 +0200

    fix black patch ID of loaded JPG

--- helpers/loading_slides.py
+++ helpers/loading_slides.py
@@ -102,7 +102,8 @@ def process_slide_jpg(slide_jpg: PIL.Image):
         for j in range(0, image_array.shape[1]-224, 224):
             total+=1
             patch = image_array[j:j+224, i:i+224, :]
-            if not np.all(patch):
+            # if patch is not fully black (i.e. rejected previously)
+            if np.sum(patch) > 0:
                 canny_norm_patch_list.append(patch)
                 coords_list.append((j,i))
                 patch_saved+=1
